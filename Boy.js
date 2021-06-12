f40dabe24803cfba5683456a6becb680278d9b79
shawnwhjr <73105843+shawnwhjr@users.noreply.github.com>
2021-06-12 18:02:30 +0530

    Add files via upload

---
+++ Boy.js
@@ -0,0 +1,20 @@
+class boy {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      this.image=loadImage("Plucking mangoes/boy.png")
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+      imageMode(CENTER);
+      fill("brown");
+      image(this.image,pos.x, pos.y, this.width, this.height);
+      pop();
+    }
+  };
